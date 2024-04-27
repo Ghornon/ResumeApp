c828a89d9e7adb989f4bea76abb23529c327ba16
Ghornon <szymon198198@gmail.com>
2024-04-27 15:16:07 +0200

    Add test Login register Page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { LoginPage } from './pages/LoginRegister';
+
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import './styles/main.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<App />
+		<BrowserRouter>
+			<Routes>
+				<Route path="/">
+					<Route index element={<LoginPage />} />
+					<Route path="/login" element={<LoginPage />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
 	</React.StrictMode>
 );
