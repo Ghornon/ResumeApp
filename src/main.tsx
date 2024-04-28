87c720bfed6d61893e98f0e1792c2c7f56be0b6b
Mateusz Dynur <mateusz.dynur@microsoft.wsei.edu.pl>
2024-04-28 12:35:59 +0200

    remove mdb react import

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './styles/main.scss';
 import { Auth } from './components/Auth';
