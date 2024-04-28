452605ae12cb353aff78f26f62b9138f6f596959
Ghornon <szymon198198@gmail.com>
2024-04-28 22:26:33 +0200

    Update NotFound page theme

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -2,8 +2,8 @@ import { CircularProgress, Grid } from '@mui/material';
 
 export const Spinner = () => {
     return (
-        <Grid container spacing={2} minHeight={'100vh'}>
-            <Grid display="flex" justifyContent="center" alignItems="center">
+        <Grid spacing={2}>
+            <Grid display="flex" justifyContent="center" alignItems="center" minHeight={'100vh'}>
                 <CircularProgress />
             </Grid>
         </Grid>
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,21 +1,37 @@
+import { Avatar, Box, Button, Container, Grid, Typography } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
 
 const NotFound = () => {
     const navigate = useNavigate();
     return (
-        <div className=" w-full h-full p-6 flex flex-col justify-center items-center gap-1">
-            <div className=" max-w-sm w-full rounded-lg px-4 py-2 ring-slate-900/5 shadow-lg">
-                <h1 className=" font-bold text-4xl flex justify-center mb-4">404</h1>
-                <p className="text-slate-400 font-bold">Ooops...</p>
-                <p className="text-slate-400">Page not found </p>
-            </div>
-            <button
-                    className="bg-black text-white
-                p-2 rounded-md"
-                    onClick={() => navigate('/')}>
-                    Back to Home
-                </button>
-        </div>
+        <Grid spacing={2}>
+            <Grid display="flex" justifyContent="center" alignItems="center" minHeight={'100vh'}>
+                <Box
+                    maxWidth="xs"
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}>
+                    <Typography component="h1" variant="h1">
+                        404
+                    </Typography>
+                    <Typography component="p">
+                        The page you are looking for was not found.
+                    </Typography>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="text"
+                        onClick={() => navigate('/')}
+                        sx={{ mt: 3, mb: 2 }}>
+                        Back to Home
+                    </Button>
+                </Box>
+            </Grid>
+        </Grid>
     );
 };
 
