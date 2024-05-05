a9e0d15fcf026c7c82c687964f7386f1720df29c
Ghornon <szymon198198@gmail.com>
2024-05-05 18:38:16 +0200

    Add custom PDF view

--- src/components/PDFView.tsx
+++ src/components/PDFView.tsx
@@ -1,120 +1,80 @@
-import { Box } from '@mui/material';
+import { useEffect, useRef, useState } from 'react';
+import { pdfjs, Document, Page } from 'react-pdf';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import 'react-pdf/dist/esm/Page/TextLayer.css';
+
+import type { PDFDocumentProxy } from 'pdfjs-dist';
+import ExampleTemplate from './templates/example/ExampleTemplate';
+import { BlobProvider } from '@react-pdf/renderer';
 import { DocumentSnapshot } from 'firebase/firestore';
-import {
-    Text,
-    Font,
-    Page,
-    View,
-    Image,
-    Document,
-    StyleSheet,
-    PDFViewer,
-} from '@react-pdf/renderer';
-import Header from './templates/example/Header';
-import Education from './templates/example/Education';
-import Skills from './templates/example/Skills';
-import Experience from './templates/example/Experience';
+import { Box, Container } from '@mui/material';
+import { blueGrey } from '@mui/material/colors';
 
-const styles = StyleSheet.create({
-    page: {
-        padding: 30,
-        width: 1000,
-    },
-    container: {
-        flex: 1,
-        flexDirection: 'row',
-        '@media max-width: 400': {
-            flexDirection: 'column',
-        },
-    },
-    image: {
-        marginBottom: 10,
-    },
-    leftColumn: {
-        flexDirection: 'column',
-        width: 170,
-        paddingTop: 30,
-        paddingRight: 15,
-        '@media max-width: 400': {
-            width: '100%',
-            paddingRight: 0,
-        },
-        '@media orientation: landscape': {
-            width: 200,
-        },
-    },
-    footer: {
-        fontSize: 12,
-        fontFamily: 'Lato Bold',
-        textAlign: 'center',
-        marginTop: 15,
-        paddingTop: 5,
-        borderWidth: 3,
-        borderColor: 'gray',
-        borderStyle: 'dashed',
-        '@media orientation: landscape': {
-            marginTop: 10,
-        },
-    },
-});
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+    'pdfjs-dist/build/pdf.worker.min.js',
+    import.meta.url,
+).toString();
 
-Font.register({
-    family: 'Open Sans',
-    src: `https://fonts.gstatic.com/s/opensans/v17/mem8YaGs126MiZpBA-UFVZ0e.ttf`,
-});
-
-Font.register({
-    family: 'Lato',
-    src: `https://fonts.gstatic.com/s/lato/v16/S6uyw4BMUTPHjx4wWw.ttf`,
-});
-
-Font.register({
-    family: 'Lato Italic',
-    src: `https://fonts.gstatic.com/s/lato/v16/S6u8w4BMUTPHjxsAXC-v.ttf`,
-});
-
-Font.register({
-    family: 'Lato Bold',
-    src: `https://fonts.gstatic.com/s/lato/v16/S6u9w4BMUTPHh6UVSwiPHA.ttf`,
-});
-
-const Resume = (props) => (
-    <Page {...props} style={styles.page}>
-        <Header />
-        <View style={styles.container}>
-            <View style={styles.leftColumn}>
-                <Image src="https://react-pdf.org/static/images/luke.jpg" style={styles.image} />
-                <Education />
-                <Skills />
-            </View>
-            <Experience />
-        </View>
-        <Text style={styles.footer}>This IS the candidate you are looking for</Text>
-    </Page>
-);
+function getWindowSize() {
+    const { innerWidth, innerHeight } = window;
+    return { innerWidth, innerHeight };
+}
 
 const PDFView = ({ resumeSnapshot }: { resumeSnapshot: DocumentSnapshot }) => {
+    const [numPages, setNumPages] = useState<number>();
+    const [windowSize, setWindowSize] = useState(getWindowSize());
+
+    function onDocumentLoadSuccess({ numPages: nextNumPages }: PDFDocumentProxy): void {
+        setNumPages(nextNumPages);
+    }
+
+    useEffect(() => {
+        function handleWindowResize() {
+            setWindowSize(getWindowSize());
+        }
+
+        window.addEventListener('resize', handleWindowResize);
+
+        return () => {
+            window.removeEventListener('resize', handleWindowResize);
+        };
+    }, []);
+
     return (
         <Box
             sx={{
                 display: 'flex',
-                flex: '1 1 0',
-                backgroundColor: 'rgb(101, 110, 131)',
-                height: '100vh',
-                width: '100%',
-                overflow: 'visible',
-                position: 'sticky',
-                top: 0,
+                alignContent: 'center',
+                justifyContent: 'center',
+                flexDirection: 'column',
+                background: blueGrey[50],
+                width: '50%',
             }}>
-            <PDFViewer width={'100%'} showToolbar={false}>
-                <Document
-                    author="Luke Skywalker"
-                    keywords="awesome, resume, start wars"
-                    subject="The resume of Luke Skywalker"
-                    title="Resume">
-                    <Resume size="A4" />
-                </Document>
-            </PDFViewer>
+            <Container
+                sx={{
+                    width: 'auto',
+                }}>
+                <Box
+                    sx={{
+                        borderRadius: 1,
+                        overflow: 'hidden',
+                        boxShadow: '0 0 10px rgba(0,0,0,0.1)',
+                    }}>
+                    <BlobProvider document={<ExampleTemplate resumeSnapshot={resumeSnapshot} />}>
+                        {({ url }) => (
+                            <Document file={url} onLoadSuccess={onDocumentLoadSuccess}>
+                                {Array.from(new Array(numPages), (el, index) => (
+                                    <Page
+                                        key={`page_${index + 1}`}
+                                        pageNumber={index + 1}
+                                        height={windowSize.innerHeight - 60}
+                                    />
+                                ))}
+                            </Document>
+                        )}
+                    </BlobProvider>
+                </Box>
+            </Container>
         </Box>
     );
 };
--- src/pages/Editor.tsx
+++ src/pages/Editor.tsx
@@ -24,7 +24,7 @@ const Editor = () => {
 
     if (resumeSnapshot)
         return (
-            <Stack sx={{ width: '100%', flexDirection: 'row', gap: '1rem', minHeight: '100vh' }}>
+            <Stack sx={{ width: '100%', flexDirection: 'row', minHeight: '100vh' }}>
                 <EditorForms resumeSnapshot={resumeSnapshot} />
                 <PDFView resumeSnapshot={resumeSnapshot} />
             </Stack>
