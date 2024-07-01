88ce94ef293858955339f1288d6c2b0b5ec9f29d
Mateusz Dynur <mateusz.dynur@microsoft.wsei.edu.pl>
2024-07-01 18:49:06 +0200

    add mobile page and refactor navbar

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,9 +1,20 @@
-import { AppBar, Toolbar, Typography, Box } from '@mui/material';
+import {
+    AppBar,
+    Toolbar,
+    Typography,
+    Box,
+    BottomNavigation,
+    BottomNavigationAction,
+    useMediaQuery,
+} from '@mui/material';
+import { InsertDriveFile, Home, Email } from '@mui/icons-material';
+import { NavLink } from 'react-router-dom';
 import HeaderContent from './HeaderContent';
 import logo from '../../../public/idea.png';
-import { NavLink } from 'react-router-dom';
 
 const Header = () => {
+    const isMobile = useMediaQuery('(max-width:960px)');
+
     return (
         <AppBar
             component="header"
@@ -16,20 +27,60 @@ const Header = () => {
                 <NavLink
                     to="/"
                     style={{
-                        flexGrow: 1,
                         display: 'flex',
-                        flexDirection: 'row',
                         alignItems: 'center',
+                        textDecoration: 'none',
+                        color: 'inherit',
                     }}>
-                    <img src={logo} alt="Logo" style={{ width: '40px', height: '100%' }} />
-                    <Typography variant="h6">CV Maker</Typography>
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                        <img src={logo} alt="Logo" style={{ width: '40px', height: 'auto' }} />
+                    </Box>
+                    <Box sx={{ ml: 2, textAlign: 'right' }}>
+                        <Typography variant="h5" sx={{ whiteSpace: 'nowrap' }}>
+                            CV Maker
+                        </Typography>
+                        <Typography
+                            variant="subtitle2"
+                            color="gray"
+                            sx={{ fontSize: '10px', mt: -1 }}>
+                            by Dreamteam
+                        </Typography>
+                    </Box>
                 </NavLink>
-                <nav style={{ display: 'flex', gap: '20px' }}>
-                    <NavLink to="/">Home</NavLink>
-                    <NavLink to="/resume-templates">Resume</NavLink>
-                    <NavLink to="/cover-letter-templates">Cover Letter</NavLink>
-                </nav>
                 <Box sx={{ flexGrow: 1 }} />
+                {isMobile ? (
+                    <BottomNavigation
+                        value={false}
+                        onChange={() => {}}
+                        sx={{ width: '100%', position: 'fixed', bottom: 0 }}
+                        showLabels
+                        component="nav">
+                        <BottomNavigationAction
+                            label="Home"
+                            icon={<Home />}
+                            component={NavLink}
+                            to="/"
+                        />
+                        <BottomNavigationAction
+                            label="Resume"
+                            icon={<InsertDriveFile />}
+                            component={NavLink}
+                            to="/resume-templates"
+                        />
+                        <BottomNavigationAction
+                            label="Cover Letter"
+                            icon={<Email />}
+                            component={NavLink}
+                            to="/cover-letter-templates"
+                        />
+                    </BottomNavigation>
+                ) : (
+                    <Box component="nav" sx={{ display: 'flex', flexGrow: 1, gap: 2 }}>
+                        <NavLink to="/">Home</NavLink>
+                        <NavLink to="/resume-templates">Resume</NavLink>
+                        <NavLink to="/cover-letter-templates">Cover Letter</NavLink>
+                    </Box>
+                )}
                 <HeaderContent />
             </Toolbar>
         </AppBar>
--- src/layout/Header/Profile.tsx
+++ src/layout/Header/Profile.tsx
@@ -1,20 +1,112 @@
+import React, { useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '../../config/firebase';
 import { signOut } from 'firebase/auth/cordova';
-import { Box, IconButton } from '@mui/material';
-import { LogoutOutlined } from '@mui/icons-material';
+import {
+    Box,
+    IconButton,
+    Avatar,
+    Menu,
+    MenuItem,
+    ListItemIcon,
+    Divider,
+    Typography,
+} from '@mui/material';
 import { Link } from 'react-router-dom';
+import { Settings, Logout } from '@mui/icons-material';
+
 const Profile = () => {
     const [user] = useAuthState(auth);
-    const logout = async () => {
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+
+    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const handleLogout = async () => {
         await signOut(auth);
     };
+
     return (
-        <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-            <Link to="/profile">{user?.displayName ? user.displayName : 'Guest'}</Link>
-            <IconButton size="large" color="secondary" onClick={logout}>
-                <LogoutOutlined />
-            </IconButton>
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Box
+                sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    cursor: 'pointer',
+                    textAlign: 'center',
+                }}
+                onClick={handleClick}>
+                <IconButton size="small" sx={{ ml: 2 }}>
+                    <Avatar
+                        sx={{ width: 40, height: 40 }}
+                        src={user?.photoURL || ''}
+                        alt={user?.displayName || 'User Avatar'}
+                    />
+                </IconButton>
+                <Typography
+                    aria-controls={open ? 'account-menu' : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? 'true' : undefined}>
+                    {user?.displayName || 'Guest'}
+                </Typography>
+            </Box>
+            <Menu
+                anchorEl={anchorEl}
+                id="account-menu"
+                open={open}
+                onClose={handleClose}
+                PaperProps={{
+                    elevation: 0,
+                    sx: {
+                        overflow: 'visible',
+                        filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                        mt: 1.5,
+                        '& .MuiAvatar-root': {
+                            width: 32,
+                            height: 32,
+                            ml: -0.5,
+                            mr: 1,
+                        },
+                        '&::before': {
+                            content: '""',
+                            display: 'block',
+                            position: 'absolute',
+                            top: 0,
+                            right: 14,
+                            width: 10,
+                            height: 10,
+                            bgcolor: 'background.paper',
+                            transform: 'translateY(-50%) rotate(45deg)',
+                            zIndex: 0,
+                        },
+                    },
+                }}
+                transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}>
+                <MenuItem onClick={handleClose} component={Link} to="/profile">
+                    <Avatar src={user?.photoURL || ''} alt={user?.displayName || 'User Avatar'} />
+                    <Typography sx={{ ml: 1 }}>Profile</Typography>
+                </MenuItem>
+                <Divider />
+                <MenuItem onClick={handleClose}>
+                    <ListItemIcon>
+                        <Settings fontSize="small" />
+                    </ListItemIcon>
+                    Settings
+                </MenuItem>
+                <MenuItem onClick={handleLogout}>
+                    <ListItemIcon>
+                        <Logout fontSize="small" />
+                    </ListItemIcon>
+                    Logout
+                </MenuItem>
+            </Menu>
         </Box>
     );
 };
