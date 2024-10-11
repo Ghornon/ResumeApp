ce4ce547ee81cb79d0b07926e2a3995d7d8b29ae
Ghornon <szymon198198@gmail.com>
2024-10-11 22:04:56 +0200

    Add bottom navigation to editor page

--- src/pages/Editor.tsx
+++ src/pages/Editor.tsx
@@ -4,21 +4,29 @@ import { useDocumentOnce } from 'react-firebase-hooks/firestore';
 import { doc } from 'firebase/firestore';
 import { Spinner } from '../components/Spinner';
 import ErrorSnackbar from '../components/ErrorSnackbar';
-import { Stack } from '@mui/material';
+import { BottomNavigation, BottomNavigationAction, Box, Stack, useMediaQuery } from '@mui/material';
 import EditorForm from '../components/EditorForm';
 import PDFPreview from '../components/PDFPreview/PDFPreview';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useResumeStore } from '../store/ResumeStore';
 import ThemeForm from '../components/ThemeForm';
+import EditNoteIcon from '@mui/icons-material/EditNote';
+import { DesignServices, PictureAsPdf } from '@mui/icons-material';
 
 const Editor = () => {
+    const [nav, setNav] = useState('EditorForm');
+    const isMobile = useMediaQuery('(max-width:1199px)');
+
+    const handleChange = (_event: React.SyntheticEvent, newValue: string) => {
+        setNav(newValue);
+        console.log(newValue);
+    };
+
     const { resumeId } = useParams();
 
     const [resumeSnapshot, resumeLoading, resumeError] = useDocumentOnce(
         doc(db, 'resumes', resumeId || ''),
     );
-
-    const resetState = useResumeStore((state) => state.reset);
 
     useEffect(() => {
         if (!resumeLoading && !resumeError) {
@@ -39,20 +47,55 @@ const Editor = () => {
                 });
             }
         }
-    }, [resumeSnapshot, resumeLoading, resumeError, useResumeStore, resetState]);
+    }, [resumeSnapshot, resumeLoading, resumeError, useResumeStore]);
 
     if (resumeLoading) return <Spinner />;
 
     if (resumeError) return <ErrorSnackbar>{resumeError.message}</ErrorSnackbar>;
 
-    if (resumeSnapshot)
+    if (isMobile)
         return (
-            <Stack direction={{ sm: 'column', lg: 'row' }}>
-                <EditorForm />
-                <PDFPreview />
-                <ThemeForm />
-            </Stack>
+            <>
+                <Stack
+                    direction={{ sm: 'column', lg: 'row' }}
+                    sx={{ paddingBottom: nav == 'PDFPreview' ? 0 : 8 }}>
+                    {nav == 'EditorForm' ? <EditorForm /> : null}
+                    {nav == 'PDFPreview' ? <PDFPreview /> : null}
+                    {nav == 'ThemeForm' ? <ThemeForm /> : null}
+                </Stack>
+
+                <BottomNavigation
+                    value={nav}
+                    onChange={handleChange}
+                    sx={{ width: '100%', position: 'fixed', bottom: 0, left: 0 }}
+                    showLabels
+                    component="nav">
+                    <BottomNavigationAction
+                        label="Forms"
+                        value="EditorForm"
+                        icon={<EditNoteIcon />}
+                    />
+                    <BottomNavigationAction
+                        label="Preview"
+                        value="PDFPreview"
+                        icon={<PictureAsPdf />}
+                    />
+                    <BottomNavigationAction
+                        label="Theme"
+                        value="ThemeForm"
+                        icon={<DesignServices />}
+                    />
+                </BottomNavigation>
+            </>
         );
+
+    return (
+        <Stack direction={{ sm: 'column', lg: 'row' }}>
+            <EditorForm />
+            <PDFPreview />
+            <ThemeForm />
+        </Stack>
+    );
 };
 
 export default Editor;
